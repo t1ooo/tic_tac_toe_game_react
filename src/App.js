0f7abc117a6bc03a7117c0125c2fd5e100ed73f1
graibn <5966878+t1ooo@users.noreply.github.com>
2019-12-02 23:26:48 +0500

    add some comment

--- src/App.js
+++ src/App.js
@@ -175,14 +175,17 @@ class App extends React.Component {
   }
 
   markCell(x,y) {
+    // someone won
     if (this.isWin(3) !== '') {
       return;
     }
     /* console.log(this.state.logStateIndex, this.state.log.length); */
+    // mark after select state
     if (this.state.logStateIndex !== this.state.log.length) {
       this.truncateLog(this.state.logStateIndex+1);
       //console.log('truncate:', this.state.logStateIndex, this.state.log.length);
     }
+    // already market
     if (this.getCellContent(x,y) !== '') {
       return;
     }
