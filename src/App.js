c40071f5c96e0cf6e989755a0ce11ec279cda512
graibn <5966878+t1ooo@users.noreply.github.com>
2019-12-02 23:23:32 +0500

    detect win

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
       <div className="App">
           {this.table(3, 3)}
           <div>
+            Wiiner: {this.isWin(3)}
             Next player: {this.state.player}
             <div>
               <li key={Math.random()}>
@@ -40,14 +41,14 @@ class App extends React.Component {
   
   changeLogStateIndex(x,y) {
     this.setState((state, props) => {
-      console.log('changeLogStateIndex', this.getCellIndex(x,y));
+      console.log('changeLogStateIndex:', this.getCellIndex(x,y));
       return {logStateIndex: this.getCellIndex(x,y)};
     });
   }
   
   incrLogStateIndex(x,y) {
     this.setState((state, props) => {
-      console.log('incrLogStateIndex', state.logStateIndex+1);
+      console.log('incrLogStateIndex:', state.logStateIndex+1);
       return {logStateIndex: state.logStateIndex+1};
     });
   }
@@ -72,12 +73,112 @@ class App extends React.Component {
         </tbody>
     </table>);
   }
+  
+  /* 
+    vertical:              0 1, 0 2, ..., x   y+1
+    horisontal:            0 0, 1 0, ..., x+1 y
+    diagonal left right:   0 0, 1 1, ..., x+1 y+1
+    diagonal right left:   3 0, 2 1, ..., x-1 y+1
+  */
+  isWin(size) {
+    const group = {
+        'x':{'x':[],'y':[]},
+        'o':{'x':[],'y':[]},
+    }
+    for(let i=0; i<this.state.log.length; i++) {
+      const v = this.state.log[i];
+      /* group[v.player] = {v.x, v.x}; */
+      group[v.player]['x'].push(v.x);
+      group[v.player]['y'].push(v.y);
+    }
+
+    function isAllEqual(arr) {
+      return (
+        arr.length !== 0
+        && arr.every(v=>(v === arr[0]))
+      );
+    }
+    
+    function isAllIncr(arr, max) {
+      for(let i=0; i<=max; i++) {
+        if (arr.indexOf(i) === -1) {
+          return false;
+        }
+      }
+      return true;
+    }
+    
+    function isDiagonalLeftRightWin(xs, ys, max) {
+      for(let i=0; i<=max; i++) {
+        if (
+          xs.indexOf(i) === -1  
+          || ys.indexOf(i) === -1 
+          || xs.indexOf(i) !== ys.indexOf(i)
+        ) {
+          return false;
+        }
+      }
+      return true;
+    }
+    
+    function isDiagonalRightLeftWin(xs, ys, max) {
+      for(let i=0,j=max; i<=max; i++,j--) {
+        if (
+          xs.indexOf(i) === -1  
+          || ys.indexOf(j) === -1 
+          || xs.indexOf(i) !== ys.indexOf(j)
+        ) {
+          return false;
+        }
+      }
+      return true;
+    }
+    
+    for(let player in group) {  
+      const gr = group[player];
+      
+      //console.log('isWin v', isAllEqual(gr['x']), isAllIncr(gr['y'], size-1), size-1, gr['x'], gr['y']);
+      //console.log('isWin h', isAllEqual(gr['y']), isAllIncr(gr['x'], size-1), size-1, gr['x'], gr['y']);
+      
+      // vertical
+      if (
+        isAllEqual(gr['x'])
+        && isAllIncr(gr['y'], size-1)
+      ) {
+          console.log('isWin:', 'vartical', player);
+          return player;
+      }
+      
+      // horisontal
+      if (
+        isAllEqual(gr['y']) 
+        && isAllIncr(gr['x'], size-1)
+      ) {
+          console.log('isWin:', 'horisontal', player);
+          return player;
+      }
+      
+      // diagonal lefr right
+      if (isDiagonalLeftRightWin(gr['x'], gr['y'], size-1)) {
+          console.log('isWin:', 'diagonal lefr right', player);
+          return player;
+      }
+      
+      // diagonal right lefr
+      if (isDiagonalRightLeftWin(gr['x'], gr['y'], size-1)) {
+          console.log('isWin:', 'diagonal right lefr', player);
+          return player;
+      }
+    }
+    
+    return '';
+  }
 
   markCell(x,y) {
     /* console.log(this.state.logStateIndex, this.state.log.length); */
     if (this.state.logStateIndex !== this.state.log.length) {
       this.truncateLog(this.state.logStateIndex+1);
-      console.log('truncate', this.state.logStateIndex, this.state.log.length);
+      //console.log('truncate:', this.state.logStateIndex, this.state.log.length);
     }
     if (this.getCellContent(x,y) !== '') {
       return;
@@ -89,25 +190,25 @@ class App extends React.Component {
   
   writeToLog(x, y) {
     this.setState((state, props) => {
-      console.log('writeToLog', state.log);
+      console.log('writeToLog:', state.log);
       return {log: state.log.concat({x:x,y:y,player:this.state.player})};
     });
   }
   
   truncateLog(size) {
     this.setState((state, props) => {
-      console.log('truncateLog', size);
+      console.log('truncateLog:', size);
       return {log: state.log.slice(0, size)};
     });
   }
 
   changePlayer() {
-    console.log('changePlayer');
     const players = {
       'x':'o',
       'o':'x',
     };
     this.setState((state, props) => {
+      console.log('changePlayer:', players[state.player]);
       return {player: players[state.player]};
     });
   }
